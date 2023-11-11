2e1556359c1c3a8db5ec066ee9e83d75425788f9
Jungyeon Seo <seocindy2002@gmail.com>
2023-11-12 01:07:01 +0900

    Feat : 유효한 결과 페이지 ui wkrdjq

--- SeeReaURL/SeeReaURL/Result/QualifiedResultViewController.swift
+++ SeeReaURL/SeeReaURL/Result/QualifiedResultViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class QualifiedResultViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var linkview: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.delegate = self
+        addContentScrollView()
+        setPageControl()
 
+        self.linkview.layer.cornerRadius = 10
+        self.scrollView.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
@@ -28,6 +39,34 @@ class QualifiedResultViewController: UIViewController, UIScrollViewDelegate {
     // Tip! 문구
     
     // 사례 화면 구현 : 스크롤 뷰
+    var images = [#imageLiteral(resourceName: "img2"), #imageLiteral(resourceName: "img1")]
+    var imageViews = [UIImageView]()
     
+    private func addContentScrollView() {
+            
+            for i in 0..<images.count {
+                let imageView = UIImageView()
+                let xPos = scrollView.frame.width * CGFloat(i)
+                imageView.frame = CGRect(x: xPos, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
+                imageView.image = images[i]
+                scrollView.addSubview(imageView)
+                scrollView.contentSize.width = imageView.frame.width * CGFloat(i + 1)
+            }
+            
+        }
+    private func setPageControl() {
+            pageControl.numberOfPages = images.count
+            
+        }
+        
+        private func setPageControlSelectedPage(currentPage:Int) {
+            pageControl.currentPage = currentPage
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let value = scrollView.contentOffset.x/scrollView.frame.size.width
+            setPageControlSelectedPage(currentPage: Int(round(value)))
+        }
+
     
 }
